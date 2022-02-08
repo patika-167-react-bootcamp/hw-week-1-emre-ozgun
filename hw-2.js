8814072baebf2d79c66d828a57e280171234df21
emre-ozgun <emre2996ozgun@gmail.com>
2022-02-08 20:25:10 +0300

    reduce run time complexity hw-2

--- hw-2.js
+++ hw-2.js
@@ -1,19 +1,6 @@
-//with native includes
 Array.prototype.includesCI = function (target) {
-	return this.map((i) => i.toLowerCase()).includes(target) ? true : false;
+	return this.some((word) => word.toLowerCase() === target.toLowerCase());
 };
 
-//without native includes method
-Array.prototype.includesCaseInsensitive = function (target) {
-	return this.some((word) => {
-		if (word.length !== target.length) {
-			return false;
-		} else {
-			return word
-				.split('')
-				.every((l, i) => l.toLowerCase() === target[i].toLowerCase());
-		}
-	});
-};
-
-console.log(['Mercedes'].includesCaseInsensitive('mErcedes'));
+console.log(['abc', 'def', 'emre'].includesCI('emRe'));
+console.log(['abc', 'def', 'emre'].includesCI('xyz'));
